84c554333a908176b92a42b1680e1f06bea6f42c
jbar <mr.jockex09@hotmail.com>
2018-12-12 18:27:23 +0100

    Lägger till testkommentar

--- app/src/main/java/com/example/anton/biotop/AnimalActivity.java
+++ app/src/main/java/com/example/anton/biotop/AnimalActivity.java
@@ -10,4 +10,6 @@ public class AnimalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
     }
+
+    // Hej tes test
 }
